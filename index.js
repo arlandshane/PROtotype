ca77deb5ad9246e2e72216a4532e27fad0c5adc1
Shane Arland <arlandshane@gmail.com>
2022-12-21 02:41:53 +0530

    second PRO

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(".card").css("opacity", "0")
 $("#navi").click(function () {
     $(".card").css("opacity", "1")
-
+    $("#navi").css("transition-duration", "500ms")
     if ($("#navi").attr("class") === "navigation-middle") {
         $("#navi").removeClass("navigation-middle").addClass("navigation-down")
     } else {
