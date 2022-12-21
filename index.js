ed1f69148c5e2749665f84e19036d82ef51a9b9d
Shane Arland <arlandshane@gmail.com>
2022-12-21 09:06:06 +0530

    added fa-brands and linked them

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ $("#navi").click(function () {
     $(".card").css("opacity", "1")
     $("#navi").css("transition-duration", "500ms")
     if ($("#navi").attr("class") === "navigation-middle") {
+        // open
         $("#navi").removeClass("navigation-middle").addClass("navigation-down")
         $(".middle").css("opacity", "0")
         $(".down").css("opacity", "1")
+        $(".closed").css("opacity", "0")
     } else {
+        // closed
         $("#navi").removeClass("navigation-down").addClass("navigation-middle")
         $(".card").css("opacity", "0")
         $(".down").css("opacity", "0")
         $(".middle").css("opacity", "1")
+        $(".closed").css("opacity", "1")
     }
 })
