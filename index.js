18ffbe2554e24ee9fbb1991fd75d8be402b16550
Shane Arland <arlandshane@gmail.com>
2022-12-21 08:39:37 +0530

    removed card numbering

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ $("#navi").click(function () {
     $("#navi").css("transition-duration", "500ms")
     if ($("#navi").attr("class") === "navigation-middle") {
         $("#navi").removeClass("navigation-middle").addClass("navigation-down")
+        $(".middle").css("opacity", "0")
+        $(".down").css("opacity", "1")
     } else {
         $("#navi").removeClass("navigation-down").addClass("navigation-middle")
         $(".card").css("opacity", "0")
+        $(".down").css("opacity", "0")
+        $(".middle").css("opacity", "1")
     }
 })
